03ae0eafa5da413837d03cbcc99efbf645c9b28e
lsahakyan <85043466+lsahakyan@users.noreply.github.com>
2021-09-01 08:19:18 +0200

    Add files via upload

---
+++ LiliT_JS1_3.js
@@ -0,0 +1,113 @@
+// Ex 5
+
+var treeNodes = [
+	{parent: null, id: 0},
+	{parent: 0, id: 1},
+	{parent: 0, id: 2},
+	{parent: 1, id: 3},
+	{parent: 1, id: 4},
+	{parent: 2, id: 5},
+	{parent: 4, id: 6},
+]
+
+
+
+function recNodes (parentId) {
+	var obj = {}
+
+	for (let i= 0; i<treeNodes.length; i++){
+        if (parentId === treeNodes[i].parent){
+        	
+       
+        	let subTree = recNodes(treeNodes[i].id)
+        	obj[treeNodes[i].id] = subTree
+        }
+           
+	}
+    return obj
+}
+recNodes(null)
+
+
+// Ex 4
+
+
+let object = {a : 1, b : 1, c: 2, d: 3, e: 3, f: 1}
+let invObject = {}
+
+for (let property in object) {
+	if(invObject[object[property]] === undefined) {
+		invObject[object[property]] = property
+	} else {
+    
+    if (!Array.isArray(invObject[object[property]])) {
+      let temp = invObject[object[property]]
+      invObject[object[property]] = []
+      invObject[object[property]].push(temp)
+		  invObject[object[property]].push(property)
+    }else {
+      invObject[object[property]].push(property)
+    }
+  }
+}
+
+console.log(invObject)
+
+
+
+
+// Ex 3
+
+
+function sumOfDigits(num){
+    let lastDigit = num % 10;
+    let sum = 0;
+    let number = (num - lastDigit) / 10;
+    if(number < 10){
+        return sum + number + lastDigit;
+    }
+    sum = sum + lastDigit + sumOfDigits(number); 
+    while( sum >= 10){
+        sum = sumOfDigits(sum);
+    }
+    return sum;
+} 
+
+
+
+
+// Ex 2
+
+function flatten(arr) {
+  const result = []
+
+  arr.forEach((i) => {
+    if (Array.isArray(i)) {
+      result.push(...flatten(i))
+    } else {
+      result.push(i)
+    }
+  })
+
+  return result
+}
+
+
+
+// Ex 1
+
+
+function removeFirstElement(arr, i = 0){
+    if(!arr.length) {
+        return arr;
+    }
+    if(i === arr.length - 1) {
+        arr.length = arr.length - 1;
+        return arr;
+    }
+    arr[i] = arr[i + 1];   
+    return removeFirstElement(arr, i + 1);
+}
+
+
+
